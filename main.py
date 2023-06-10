6561c111dc3437a00f597034b3e6e4c35047f6a4
Kiril Spiridonov <kirilspiridonov@abv.bg>
2023-06-10 12:05:52 +0300

    added some functions

--- main.py
+++ main.py
@@ -6,3 +6,20 @@ def count_el(lst: list) -> dict:
         else:
             d[el] = 1
     return d
+
+# In: ['a', 'a', 'c', 'c', 'b']
+# Out: (a, 2, c, 2, b, 1)
+
+def count_in_t(lst: list) -> tuple:
+    outp = ()
+    # outp = []
+    d = count_el(lst)
+    for key, value in d.items():
+        outp += (key, value)
+        # outp.append(key)
+        # outp.append(value)
+    return tuple(outp)
+
+print(
+    count_in_t(['a', 'a', 'c', 'c', 'b'])
+)
